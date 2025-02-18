941ad06ad19293073c39a1c803b92f0fcc08ea1d
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-02-18 15:44:29 +0530

    implemented redux store

---
+++ src/utils/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { use } from "react";
+
+
+const userSlice = createSlice(
+    {
+        name:"User",
+        initialState:null,
+        reducers : {
+            addUser:(state,action)=>{
+              return action.payload
+            },
+            removeUser:(state,action)=>{
+             return null
+            }
+        }
+
+    }
+)
+
+export const {addUser,removeUser} = userSlice.actions
+
+export default userSlice.reducer
