f168980570203b9d0e3d22a5b31cbbca3945b22d
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-02-20 16:45:09 +0530

    implemented signout functionality

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './Header'
 
 const Browse = () => {
   return (
-    <div>browse</div>
+    <div className='w-full'>
+      <Header/>
+    </div>
   )
 }
 
