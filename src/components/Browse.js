5be20e96cc7ef212e018c615e6ddb3cf3c4c3170
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-02-22 17:02:18 +0530

    fetched TMDB movie data with API key

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS } from '../utils/constants'
 
 const Browse = () => {
+  const getMovies = async()=>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS)
+    const json = data.json()
+    console.log(json)
+  }
+  useEffect(()=>{
+
+    getMovies()
+
+  },[])
   return (
     <div className='w-full'>
       <Header/>
+
     </div>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_produ
 export const USER_ICON = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
 
 export const BG_IMG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_small.jpg"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGE3M2VjMWI3ODFmNTIyYTE3NTYxNGUyYzVmOGQyNSIsIm5iZiI6MTc0MDIxOTk2Mi43NjE5OTk4LCJzdWIiOiI2N2I5YTYzYTU1ZGQ4MTJmYWQ0NjU0NTciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.B2KvZewTigu6Q7JKQS5F-gM-i1-r-zqx0TYWiXvlKNw'
+    }
+  };
