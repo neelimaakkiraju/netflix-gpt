f7e944560c785e63fcc914f5646dc82d8d41ca0e
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-02-22 11:20:38 +0530

    completed authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { signOut } from "firebase/auth";
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 
 const Header = () => {
 
-
+const user = useSelector((store)=>store.user)
 const navigate = useNavigate()
   const handleSignOut = ()=>{
     signOut(auth).then(() => {
@@ -21,11 +21,13 @@ const navigate = useNavigate()
   return (
     <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-full display flex justify-between'>
        <img src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" className='w-44'/>
-    
-    <div className='display flex p-2'>
-      <img src="https://cdn.vectorstock.com/i/1000v/74/41/white-user-icon-vector-42797441.jpg" alt="user-icon" className='w-10 h-10 rounded-full m-2'/>
-      <button className='bg-gray-500 h-10 p-2 rounded-md m-2 hover:bg-red-600 ' onClick={handleSignOut}>Sign Out</button>
-    </div>
+    {
+      
+    user&&<div className='display flex p-2'>
+    <img src={user.photoURL} alt="user-icon" className='w-10 h-10 rounded-full m-2'/>
+    <button className='bg-gray-500 h-10 p-2 rounded-md m-2 hover:bg-red-600 ' onClick={handleSignOut}>Sign Out</button>
+  </div>
+    }
     
     
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,58 +1,86 @@
 import React from "react";
 import Header from "./Header";
-import { useState,useRef} from "react";
+import { useState, useRef } from "react";
 import { checkIsValid } from "../utils/validate";
-import { createUserWithEmailAndPassword ,  signInWithEmailAndPassword} from "firebase/auth";
-import {auth} from "../utils/firebase"
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice.js";
 
 const Login = () => {
   const [isSignin, setIsSignIn] = useState(true);
-  const [errorMsg,setErrMsg]= useState(null)
-  const navigate = useNavigate()
-  const email = useRef(null)
-  const password = useRef(null)
+  const [errorMsg, setErrMsg] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleSubmit = () => {
+    const message = checkIsValid(email.current.value, password.current.value);
+    setErrMsg(message);
+    if (errorMsg) return;
+    if (!isSignin) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL:
+              "https://cdn.vectorstock.com/i/1000v/74/41/white-user-icon-vector-42797441.jpg",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrMsg(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrMsg(errorCode + "-" + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrMsg(errorCode + "-" + errorMessage);
+        });
+    }
+  };
   const handleForm = () => {
     setIsSignIn(!isSignin);
   };
-  const handleSubmit =()=>{
-   const message = checkIsValid(email.current.value,password.current.value)
-   setErrMsg(message)
-   if(errorMsg)return
-   if(!isSignin){
-    createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
-  .then((userCredential) => {
-    
-    const user = userCredential.user;
-    console.log(user)
-    navigate("/browse")
-   
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setErrMsg(errorCode+"-"+errorMessage)
-    
-  });
-
-   }else{
-    signInWithEmailAndPassword(auth, email.current.value, password.current.value)
-  .then((userCredential) => {
-     
-    const user = userCredential.user;
-    console.log(user)
-    navigate("/browse")
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setErrMsg(errorCode + "-"+errorMessage)
-
-  });
-    
-   }
-  }
   return (
     <div>
       <Header />
@@ -62,12 +90,16 @@ const Login = () => {
           alt="logo"
         />
       </div>
-      <form onSubmit={(e)=>e.preventDefault()} className="absolute bg-black w-4/12 my-16 mx-auto left-0 right-0 rounded-lg p-12 text-white bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute bg-black w-4/12 my-16 mx-auto left-0 right-0 rounded-lg p-12 text-white bg-opacity-80"
+      >
         <h1 className="text-3xl  font-bold py-4">
           {!isSignin ? "Sign Up" : "Sign In"}
         </h1>
         {!isSignin && (
           <input
+            ref={name}
             required
             type="text"
             placeholder="Full Name"
@@ -86,22 +118,29 @@ const Login = () => {
           placeholder="password"
           className="p-2 my-2 w-full rounded-sm bg-gray-600"
         />
-        <p className="text-red-700 font-bold">
-          {errorMsg}
-        </p>
-        <button className="p-3 my-4 bg-red-600 w-full rounded-md font-bold" onClick={handleSubmit}>
+        <p className="text-red-700 font-bold">{errorMsg}</p>
+        <button
+          className="p-3 my-4 bg-red-600 w-full rounded-md font-bold"
+          onClick={handleSubmit}
+        >
           {!isSignin ? "Sign Up" : "Sign In"}
         </button>
         <p onClick={handleForm} className="p-1 my-2">
           {!isSignin ? (
             <>
               Already Registered?{" "}
-              <span className="text-blue-500 underline cursor-pointer">SignIn</span> Now
+              <span className="text-blue-500 underline cursor-pointer">
+                SignIn
+              </span>{" "}
+              Now
             </>
           ) : (
             <>
               New to Netflix?{" "}
-              <span className="text-blue-500 underline cursor-pointer">SignUp</span> Now
+              <span className="text-blue-500 underline cursor-pointer">
+                SignUp
+              </span>{" "}
+              Now
             </>
           )}
         </p>
